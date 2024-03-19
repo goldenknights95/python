46d94a48c786eba0aab860e1f35074e1f14b4b79
goldenknights95 <158261519+goldenknights95@users.noreply.github.com>
2024-03-19 18:29:57 +0530

    Add files via upload
    defining and calling of function

---
+++ fun2.py
@@ -0,0 +1,9 @@
+def get_name():
+   print(input('what is your name?'))
+def display():
+    print('your name is{}'.format(input) )
+def get_display():
+    get_name()
+    display()
+
+get_display()
