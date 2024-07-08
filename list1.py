c7fdd6d6ac472bbec31d79a9002efd3fd86deef3
Atharv Ugale <158261519+goldenknights95@users.noreply.github.com>
2024-07-08 16:03:45 +0530

    list in python
    operation in list

---
+++ list1.py
@@ -0,0 +1,14 @@
+Myname=["aniket","atharv","om"]
+print(Myname[1])
+print(Myname[-1])
+
+MyNum=[1,2,3,4,5,6,7]
+
+print(MyNum[4:])
+print(MyNum[:5])
+print(MyNum[2:5])
+print(MyNum[-4:])
+print(MyNum[-3:-2])
+
+if 2 in MyNum:
+    print("hello")
